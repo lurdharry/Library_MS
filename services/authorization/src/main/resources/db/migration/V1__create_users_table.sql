e06866ae14eddc2f1ff49d82c659eeebde920868
lurdharry <azeezhammed57@gmail.com>
2025-03-11 21:58:07 +0100

    add migration

---
+++ services/authorization/src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,7 @@
+-- Create users table
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL
+);
