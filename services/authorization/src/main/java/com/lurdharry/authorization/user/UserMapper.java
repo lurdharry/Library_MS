b40096302f536153341b800c98e1162613ebd953
lurdharry <azeezhammed57@gmail.com>
2025-03-11 21:57:59 +0100

    add user repo and util

---
+++ services/authorization/src/main/java/com/lurdharry/authorization/user/UserMapper.java
@@ -0,0 +1,22 @@
+package com.lurdharry.authorization.user;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserMapper {
+
+    private final PasswordEncoder encoder;
+
+    public User toUserWithEncodedPassword (@Valid  UserRequest request){
+
+        return User.builder()
+                .email(request.email())
+                .password(encoder.encode(request.password()))
+                .email(request.email())
+                .build();
+    }
+}
---
+++ services/authorization/src/main/java/com/lurdharry/authorization/user/UserRequest.java
@@ -0,0 +1,17 @@
+package com.lurdharry.authorization.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+public record UserRequest(
+        @NotNull(message = "Email is required")
+        @Email(message = "Email is not valid")
+        String email,
+
+        @NotNull(message = "Password is required")
+        String password,
+
+        @NotNull(message = "Role is required")
+        Role role
+) {
+}
