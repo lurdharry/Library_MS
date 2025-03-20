a89dd1821be4725ae470ad6c201a481f159c3bb1
lurdharry <azeezhammed57@gmail.com>
2025-03-20 17:19:04 +0100

    add book util

--- services/book/src/main/java/com/lurdharry/book/book/BookMapper.java
+++ services/book/src/main/java/com/lurdharry/book/book/BookMapper.java
@@ -1,8 +1,12 @@
 package com.lurdharry.book.book;
 
 import com.lurdharry.book.dto.BookRequest;
+import com.lurdharry.book.dto.UpdateBookRequest;
 import jakarta.validation.Valid;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Consumer;
 
 @Service
 public class BookMapper {
@@ -17,14 +21,38 @@ public class BookMapper {
     }
 
 
-    public BookResponse fromCustomer(Book savedBook) {
+    public BookResponse fromBook(Book savedBook) {
         return BookResponse.builder()
                 .id(savedBook.getId())
                 .author(savedBook.getAuthor())
                 .title(savedBook.getTitle())
                 .quantity(savedBook.getQuantity())
                 .borrowedCopies(savedBook.getBorrowedCopies())
-                .availableCopies(savedBook.getAvailableCopies())
+                .build();
+    }
+
+    public Book mergeBook(Book book, @Valid UpdateBookRequest request) {
+            updateIfNotBlank(book::setAuthor,request.author());
+            updateIfNotBlank(book::setTitle,request.title());
+            if (request.newCopiesCount() > 0){
+                book.setQuantity(book.getQuantity() + request.newCopiesCount());
+            }
+            return book;
+    }
+
+    private void updateIfNotBlank(Consumer<String> setter, String value) {
+        if (StringUtils.isNotBlank(value)) {
+            setter.accept(value);
+        }
+    }
+
+    public BookBorrowResponse toBorrowBookResponse(Book book) {
+        return BookBorrowResponse.builder()
+                .id(book.getId())
+                .author(book.getAuthor())
+                .borrowedCopies(book.getBorrowedCopies())
+                .quantity(book.getQuantity())
+                .title(book.getTitle())
                 .build();
     }
 }
