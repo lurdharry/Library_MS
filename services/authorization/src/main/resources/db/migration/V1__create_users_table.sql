bb3df5507ae9d2ce1eb2979e131580bf075f03bc
lurdharry <azeezhammed57@gmail.com>
2025-03-26 11:09:35 +0100

    update migration files

--- services/authorization/src/main/resources/db/migration/V1__create_users_table.sql
+++ services/authorization/src/main/resources/db/migration/V1__create_users_table.sql
@@ -5,6 +5,5 @@ CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    role VARCHAR(50) NOT NULL
+    password VARCHAR(255) NOT NULL
 );
