6f1af7dd54251523fd4d8003490c987c9e943835
lurdharry <azeezhammed57@gmail.com>
2025-03-26 11:06:59 +0100

    add auth controller

--- services/authorization/src/main/java/com/lurdharry/authorization/auth/AuthController.java
+++ services/authorization/src/main/java/com/lurdharry/authorization/auth/AuthController.java
@@ -1,31 +1,26 @@
 package com.lurdharry.authorization.auth;
 
 import com.lurdharry.authorization.dto.ResponseDTO;
-import com.lurdharry.authorization.security.CustomUserDetails;
+import com.lurdharry.authorization.dto.TokenResponse;
 import com.lurdharry.authorization.user.UserRequest;
-import com.lurdharry.authorization.util.JwtUtil;
+import com.lurdharry.authorization.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.core.Authentication;
-
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
-    private final AuthService service;
-    private final JwtUtil jwtUtil;
+    private final UserService service;
+    private final AuthService authService;
+
 
     @PostMapping("/register")
     public ResponseEntity<ResponseDTO> register (
@@ -41,21 +36,22 @@ public class AuthController {
           return new ResponseEntity<>(response,HttpStatus.OK);
     }
 
-
     @PostMapping("/login")
-    public ResponseEntity<ResponseDTO> login(
-            @RequestBody @Valid LoginRequest request
-    ){
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(request.email(),request.password())
-        );
+    public ResponseEntity<TokenResponse> login(@RequestBody @Valid UserRequest request){
 
-        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
-
-        String token = jwtUtil.generateToken(userDetails.getUsername(),userDetails.getRole());
-
-        ResponseDTO response = new ResponseDTO(HttpStatus.OK,"Login successful!", Map.of("token",token));
+       var response = authService.authorizeLogin(request);
 
         return new ResponseEntity<>(response,HttpStatus.OK);
     }
+
+    @PostMapping("/refreshToken")
+    public ResponseEntity<Object> refreshToken(
+            @RequestBody @Valid RefreshRequest request
+    ){
+        var response = authService.refreshToken(request);
+
+        return new ResponseEntity<>(response,HttpStatus.OK);
+    }
+
+
 }
