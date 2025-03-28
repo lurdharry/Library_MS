d5c73dd86845f1c4ed5ddc25994082453bbf812a
lurdharry <azeezhammed57@gmail.com>
2025-03-28 17:08:46 +0100

    set up jwt verification in gateway

--- services/gateway/src/main/java/com/lurdharry/gateway/security/Config.java
+++ services/gateway/src/main/java/com/lurdharry/gateway/security/Config.java
@@ -1,35 +1,50 @@
 package com.lurdharry.gateway.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
-
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 
 
 @Configuration
 @EnableWebFluxSecurity
+@RequiredArgsConstructor
 public class Config {
 
+
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http, JwtUtil jwtUtil) {
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 // We use a stateless security context
                 .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
                 .authorizeExchange(exchange -> exchange
                         // Permit public endpoints (if any)
-                        .pathMatchers("/auth/register", "/auth/login").permitAll()
+                        .pathMatchers("/auth/**","/.well-known/**").permitAll()
                         // All other endpoints must be authenticated
                         .anyExchange().authenticated()
+                ) .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt ->
+                        jwt.jwtDecoder(jwtDecoder()))
                 );
-                // Add our custom JWT filter at the authentication phase
-        http.addFilterAt(new JwtAuthFilter(jwtUtil), SecurityWebFiltersOrder.AUTHENTICATION);
+
         return http.build();
     }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        String jwksUri = "http://localhost:8070/.well-known/jwks.json";
+        // Custom JWT Processor to handle algorithm and key matching
+
+        return NimbusReactiveJwtDecoder
+                .withJwkSetUri(jwksUri)
+                .build();
+    }
+
 
 
 }
