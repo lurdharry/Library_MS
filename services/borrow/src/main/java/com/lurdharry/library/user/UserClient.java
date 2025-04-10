bad11b426395d7f98064474f40f058568975e2be
lurdharry <azeezhammed57@gmail.com>
2025-04-10 23:02:40 +0100

    add feign client details

---
+++ services/borrow/src/main/java/com/lurdharry/library/user/UserClient.java
@@ -0,0 +1,27 @@
+package com.lurdharry.library.user;
+
+import com.lurdharry.library.dto.ResponseDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.UUID;
+
+
+@FeignClient(
+        name = "user-service",
+        url = "${application.config.user-url}"
+)
+public interface UserClient {
+
+    /**
+     * Calls the user service to retrieve user information by user ID.
+     *
+     * @param userId The ID of the user.
+     * @return A ResponseDTO containing the user details.
+     */
+    @GetMapping("/${user-id}")
+    ResponseDTO getUserById(
+            @PathVariable("user-id")UUID userId
+    );
+}
---
+++ services/borrow/src/main/java/com/lurdharry/library/user/UserResponse.java
@@ -0,0 +1,18 @@
+package com.lurdharry.library.user;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+
+import java.util.UUID;
+
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record UserResponse(
+        UUID id,
+        String email,
+        String firstname,
+        String lastname,
+        Role role,
+        boolean verified
+) {
+}
