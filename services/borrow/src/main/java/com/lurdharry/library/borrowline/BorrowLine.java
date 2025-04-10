ed891ed3c18bd52d3a1aa23aa10b261456fa388c
lurdharry <azeezhammed57@gmail.com>
2025-04-10 23:02:58 +0100

    add entities

--- services/borrow/src/main/java/com/lurdharry/library/borrowline/BorrowLine.java
+++ services/borrow/src/main/java/com/lurdharry/library/borrowline/BorrowLine.java
@@ -1,4 +1,24 @@
 package com.lurdharry.library.borrowline;
 
+import com.lurdharry.library.borrow.BorrowOrder;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
 public class BorrowLine {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "borrow_id")
+    private BorrowOrder borrowOrder;
+
+    private String bookId;
 }
