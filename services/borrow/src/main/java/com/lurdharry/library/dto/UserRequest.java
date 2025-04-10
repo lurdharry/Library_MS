3093f758d1c4202e6ba699db6cbebb9f2af695df
lurdharry <azeezhammed57@gmail.com>
2025-04-10 23:02:48 +0100

    add dtos

---
+++ services/borrow/src/main/java/com/lurdharry/library/dto/UserRequest.java
@@ -0,0 +1,17 @@
+package com.lurdharry.library.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+public record UserRequest(
+        @NotNull(message = "user id is required")
+        UUID userId,
+
+        @NotNull(message = "firstname is required")
+        String firstname,
+
+        @NotNull(message = "lastname is required")
+        String lastname
+) {
+}
