e14460d84add8f8500db4876a27c3fc1b35c1d99
lurdharry <azeezhammed57@gmail.com>
2025-06-24 14:33:39 +0100

    chore: add borrow book controller

--- services/borrow/src/main/java/com/lurdharry/library/borrow/BorrowController.java
+++ services/borrow/src/main/java/com/lurdharry/library/borrow/BorrowController.java
@@ -3,6 +3,7 @@ package com.lurdharry.library.borrow;
 import com.lurdharry.library.dto.ResponseDTO;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,17 +16,13 @@ public class BorrowController {
 
     private final BorrowService borrowService;
 
-//    @GetMapping("/{user-id}")
-//    public ResponseEntity<ResponseDTO> getUser(@PathVariable("user-id") String userId) {
-//        ResponseDTO response = borrowService.fetchUser(userId);
-//        return ResponseEntity.ok(response);
-//    }
 
     @PostMapping
     public ResponseEntity<ResponseDTO> borrowBook(
             @RequestBody @Valid BorrowRequest request
     ) {
-        ResponseDTO response = borrowService.borrowBook(request);
+        String res = borrowService.orderBook(request);
+        ResponseDTO response = new ResponseDTO(HttpStatus.CREATED.value(), "Book borrow successfully",res);
         return ResponseEntity.ok(response);
     }
 }
