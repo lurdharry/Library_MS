3b6322d936fad32135b5118c22233e94bccdd2e9
lurdharry <azeezhammed57@gmail.com>
2025-06-30 15:44:33 +0100

    refactor: add notification service

---
+++ services/notification/src/test/java/com/lurdharry/library/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lurdharry.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
